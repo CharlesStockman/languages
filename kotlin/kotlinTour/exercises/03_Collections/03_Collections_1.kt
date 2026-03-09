79966627787a8a0f3f8dd60341ab8e212df867b5
CharlesStockman <chuck.stockman@yahoo.com>
2026-03-08 23:18:43 -0400

    Completed two exercises on collections

---
+++ kotlin/kotlinTour/exercises/03_Collections/03_Collections_1.kt
@@ -0,0 +1,25 @@
+package exercises.`03_Collections`
+
+/**
+ * Count the number of items in all the list provided ot a function
+ *
+ * @param lists   A variable amount of list
+ * The total number of items in the list that are passed in.
+ */
+fun countItemsInAllList(vararg items: List<Int>) : Int {
+    return items.sumOf { it.size }
+}
+
+fun main() {
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(5, 22)
+    val blueNumbers = emptyList<Int>()
+
+    // The declaration with a ? can have null
+    // asserts(countItemsInAllList(null) == 0 )
+
+    assert(countItemsInAllList(blueNumbers) == 0)
+    assert(countItemsInAllList(greenNumbers) == 3)
+    assert(countItemsInAllList(greenNumbers, redNumbers) == 5)
+    println("All test passed")
+}
